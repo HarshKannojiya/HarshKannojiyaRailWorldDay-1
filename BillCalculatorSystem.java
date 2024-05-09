3a28c9afd8073c35b55786e387a1ae1e5087b568
HarshKannojiya <harshkannojiya717@gmail.com>
2024-05-09 12:33:24 +0530

    may 8 all task submission

---
+++ BillCalculatorSystem.java
@@ -0,0 +1,14 @@
+public class BillCalculatorSystem {
+
+	public static void main(String[] args) {
+	int unit = 210;
+	int BillRupees = unit<=100?
+			unit*2:(unit>=100 && unit<=150?
+			(unit-100)*3+(100*2):(unit>=151 && unit<=200?
+			(unit-150)*4+(50*3)+(100*2):(unit>=201 && unit<=250?
+			(unit-200)*5+(50*4)+(50*3)+(100*2):(unit>=251 && unit<=300?
+			(unit-250)*6+((50*5)+(50*4)+(50*3+(100*2))):(unit>=301 && unit<=500?
+			(unit-300)*7+((50*6)+(50*5)+(50*4)+(50*3)+(100*2)):(unit-200)*8+(50*7)+(50*6)+(50*5)+(50*4)+(50*3)+(50*2))))));
+	System.out.println(BillRupees);
+	}
+}
---
+++ May8/Student.java
@@ -0,0 +1,53 @@
+package May8;
+
+public class Student {
+
+	private String name;
+	private School school;
+	private Subject subject;
+	
+	public Student() {
+		
+	}
+	public Student(String name,School school,Subject subject) {
+		this.name = name;
+		this.school=school;
+		this.subject=subject;
+		
+	}
+	
+	public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public School getSchool() {
+        return school;
+    }
+
+    public void setSchool(School school) {
+        this.school = school;
+    }
+    
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", School=" + school +
+                ", Subject=" + subject +
+                '}';
+    }
+
+
+}
